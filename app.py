3e8b513337d001544307e6abd78a4fd5842c27fc
Helmar <helmar.israel@g.msuiit.edu.ph>
2021-10-04 15:17:46 +0800

    hello

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request, session
 from flask_session import Session
 import mysql.connector
 
+#hello
+
 db = mysql.connector.connect(
     host="localhost",
     user="root",
